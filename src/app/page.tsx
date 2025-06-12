491f502f3840d1f5db5a66a74d00481efed22b8b
shakilmonsi <107542773+shakilmonsi@users.noreply.github.com>
2025-06-12 14:38:46 +0600

    add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,6 @@
+// app/page.tsx
+export const dynamic = "force-dynamic";
+
 import AddTodo from "@/components/todos/AddTodo";
 import Todo from "@/components/todos/Todo";
 import { prisma } from "@/utils/prisma";
@@ -20,7 +23,7 @@ async function getData() {
 export default async function Home() {
   const data = await getData();
   return (
-    <div className=" w-screen py-20 flex justify-center flex-col items-center">
+    <div className="w-screen py-20 flex justify-center flex-col items-center">
       <span className="text-4xl font-extrabold uppercase">Todo App</span>
       <h1 className="text-5xl font-extrabold uppercase mb-5 text-center">
         <span className="lowercase">w/</span>Server Actions
@@ -28,10 +31,9 @@ export default async function Home() {
 
       <div className="flex justify-center flex-col items-center">
         <AddTodo />
-
         <div className="flex flex-col gap-5 items-center justify-center mt-10 w-screen">
-          {data.map((todo, id) => (
-            <div className="w-full" key={id}>
+          {data.map((todo) => (
+            <div className="w-full" key={todo.id}>
               <Todo todo={todo} />
             </div>
           ))}
